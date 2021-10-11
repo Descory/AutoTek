bec6e8a37eb0f6284981a8accd19b3d110524802
Descory <simas0315@gmail.com>
2021-10-11 21:45:08 +0300

    ahai

---
+++ app/Http/Controllers/api.php
@@ -0,0 +1,274 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class api extends Controller
+{
+    ///////////////////////////////////////
+    //// Gidai
+    ///////////////////////////////////////
+
+    function giduList()
+    {
+        $obj = [
+            'id' => 1,
+            'simptomai' => 'Neuzsiveda',
+            'modelis' => 'Audi A3',
+            'problema' => 'Sugedusi uzdegimo zvake',
+            'sprendimas' => 'Uzdegimo zvakes pakeitimas'
+        ];
+        $obj2 = [
+            'id' => 2,
+            'simptomai' => 'Barskejimas po sedyne',
+            'modelis' => 'Ford Mondeo mk1',
+            'problema' => 'Atsilaisvines sedynes varzas',
+            'sprendimas' => 'Varzto priverzimas'
+        ];
+
+        $body = [
+            $obj,
+            $obj2
+        ];
+        return response()->json($body, 200);
+    }
+
+    function addGidas(Request $request)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        $body = [
+            'id' => 3,
+            'simptomai' => $request->simptomai,
+            'modelis' => 'Ford Mondeo mk1',
+            'problema' => 'Atsilaisvines sedynes varzas',
+            'sprendimas' => 'Varzto priverzimas'
+        ];
+        return response()->json($body, 201);
+    }
+
+    function getGidas(Request $request, $gido_id)
+    {
+        if ($gido_id != 1)
+            return response()->json([
+                'message' => 'No "gidas" with id = ' . $gido_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'simptomai' => 'Neuzsiveda',
+            'modelis' => 'Audi A3',
+            'problema' => 'Sugedusi uzdegimo zvake',
+            'sprendimas' => 'Uzdegimo zvakes pakeitimas'
+        ];
+
+
+        return response()->json($body, 200);
+    }
+
+    function updateGidas(Request $request, $gido_id)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        if ($gido_id != 1)
+            return response()->json([
+                'message' => 'No "gidas" with id = ' . $gido_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'simptomai' => $request->simptomai,
+            'modelis' => 'Audi A3',
+            'problema' => 'Sugedusi uzdegimo zvake',
+            'sprendimas' => 'Uzdegimo zvakes pakeitimas'
+        ];
+        return response()->json($body, 200);
+    }
+
+    function deleteGidas(Request $request, $gido_id)
+    {
+        if ($gido_id != 1)
+            return response()->json([
+                'message' => 'No "gidas" with id = ' . $gido_id,
+            ], 404);
+
+        return response()->json('', 204);
+    }
+
+    ///////////////////////////////////////
+    //// Automobiliai
+    ///////////////////////////////////////
+
+    function automobiliuList()
+    {
+        $obj = [
+            'id' => 1,
+            'modelis' => 'Audi A3'
+        ];
+        $obj2 = [
+            'id' => 2,
+            'modelis' => 'Ford Mondeo mk1'
+        ];
+
+        $body = [
+            $obj,
+            $obj2
+        ];
+        return response()->json($body, 200);
+    }
+
+    function addAutomobilis(Request $request)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        $body = [
+            'id' => 3,
+            'modelis' => $request->modelis
+        ];
+        return response()->json($body, 201);
+    }
+
+    function getAutomobilis(Request $request, $automobilio_id)
+    {
+        if ($automobilio_id != 1)
+            return response()->json([
+                'message' => 'No "automobilis" with id = ' . $automobilio_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'modelis' => 'Audi A3',
+        ];
+
+
+        return response()->json($body, 200);
+    }
+
+    function updateAutomobilis(Request $request, $automobilio_id)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        if ($automobilio_id != 1)
+            return response()->json([
+                'message' => 'No "automobilis" with id = ' . $automobilio_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'modelis' =>  $request->modelis,
+        ];
+        return response()->json($body, 200);
+    }
+
+    function deleteAutomobilis(Request $request, $automobilio_id)
+    {
+        if ($automobilio_id != 1)
+            return response()->json([
+                'message' => 'No "automobilis" with id = ' . $automobilio_id,
+            ], 404);
+
+        return response()->json('', 204);
+    }
+
+    ///////////////////////////////////////
+    //// Mechanikai
+    ///////////////////////////////////////
+
+    function mechanikuList()
+    {
+        $obj = [
+            'id' => 1,
+            'vardas' => 'Mantas'
+        ];
+        $obj2 = [
+            'id' => 2,
+            'vardas' => 'Jeremajus'
+        ];
+
+        $body = [
+            $obj,
+            $obj2
+        ];
+        return response()->json($body, 200);
+    }
+
+    function addMechanikas(Request $request)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        $body = [
+            'id' => 3,
+            'vardas' => $request->vardas
+        ];
+        return response()->json($body, 201);
+    }
+
+    function getMechanikas(Request $request, $mechaniko_id)
+    {
+        if ($mechaniko_id != 1)
+            return response()->json([
+                'message' => 'No "mechanikas" with id = ' . $mechaniko_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'vardas' => 'Mantas',
+        ];
+
+
+        return response()->json($body, 200);
+    }
+
+    function updateMechanikas(Request $request, $mechaniko_id)
+    {
+        if (!$this->checkIfJsonValid($request) || $request->getContentType() != "json") {
+            return response()->json([
+                'message' => 'The request is not a valid JSON.',
+            ], 400);
+        }
+        if ($mechaniko_id != 1)
+            return response()->json([
+                'message' => 'No "mechanikas" with id = ' . $mechaniko_id
+            ], 404);
+
+        $body = [
+            'id' => 1,
+            'vardas' =>  $request->vardas,
+        ];
+        return response()->json($body, 200);
+    }
+
+    function deleteMechanikas(Request $request, $mechaniko_id)
+    {
+        if ($mechaniko_id != 1)
+            return response()->json([
+                'message' => 'No "mechanikas" with id = ' . $mechaniko_id,
+            ], 404);
+
+        return response()->json('', 204);
+    }
+
+    function checkIfJsonValid(Request $request)
+    {
+        if (empty($request->json()->all())) {
+            return false;
+        }
+        return true;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//http://127.0.0.1:8000/api/gidai
+Route::get('gidai', [api::class, 'giduList']);
+Route::post('gidai', [api::class, 'addGidas']);
+
+//http://127.0.0.1:8000/api/gidai/1
+Route::get('gidai/{gido_id}', [api::class, 'getGidas']);
+Route::put('gidai/{gido_id}', [api::class, 'updateGidas']);
+Route::delete('gidai/{gido_id}', [api::class, 'deleteGidas']);
+
+
+
+//http://127.0.0.1:8000/api/automobiliai
+Route::get('automobiliai', [api::class, 'automobiliuList']);
+Route::post('automobiliai', [api::class, 'addAutomobilis']);
+
+//http://127.0.0.1:8000/api/automobiliai/1
+Route::get('automobiliai/{automobilio_id}', [api::class, 'getAutomobilis']);
+Route::put('automobiliai/{automobilio_id}', [api::class, 'updateAutomobilis']);
+Route::delete('automobiliai/{automobilio_id}', [api::class, 'deleteAutomobilis']);
+
+
+
+//http://127.0.0.1:8000/api/mechanikai
+Route::get('mechanikai', [api::class, 'mechanikuList']);
+Route::post('mechanikai', [api::class, 'addMechanikas']);
+
+//http://127.0.0.1:8000/api/mechanikai/1
+Route::get('mechanikai/{mechaniko_id}', [api::class, 'getMechanikas']);
+Route::put('mechanikai/{mechaniko_id}', [api::class, 'updateMechanikas']);
+Route::delete('mechanikai/{mechaniko_id}', [api::class, 'deleteMechanikas']);
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::fallback(function () {
+    return response()->json(['error' => 'Not Found!'], 404);
 });
